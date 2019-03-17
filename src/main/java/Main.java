d6511a6e8a427dde3e47d1c8e3b2a61ee6cc8246
Thomas <mail@thomasdahl.one>
2019-03-17 16:20:53 +0100

    Added draft of proper children.csv Renamed kid.java to child.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,24 @@
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Path path = Paths.get("src/main/resources/testA.csv");
+        Path path = Paths.get("src/main/resources/children.csv");
         CSV_Handler csv_Handler = new CSV_Handler();
+        List<String[]> values;
 
         try {
-            csv_Handler.readCSV(path);
+            values = csv_Handler.readCSV(path);
+
+            for(String[] arr : values) {
+                System.out.println(Arrays.toString(arr));
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
